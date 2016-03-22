56438bbec80baa30ace1b87728ffb899867b8517
siddharthpant92 <siddharthpant92@gmail.com>
2016-03-22 15:50:59 -0600

    Code - Searching for paths

--- FordFulkerson_MaxFlow.py
+++ FordFulkerson_MaxFlow.py
@@ -1,11 +1,15 @@
-class createEdge(object):
+class Edge(object):
     def __init__(self, vertex1, vertex2, edgeCapacity):
-        self.vertex1 = vertex1
-        self.vertex2=vertex2
-        self.edgeCapacity = edgeCapacity
+        if(vertex1!=vertex2):
+            self.vertex1 = vertex1
+            self.vertex2=vertex2
+            self.edgeCapacity = edgeCapacity
+        elif(vertex1==vertex2):
+            print("ERROR: check graph for loops")
+            exit()
     def __repr__(self): #used to print objects.
         print
-        return "edge: %s->%s; capacity: %s; flow: %s" %(self.vertex1, self.vertex2, self.capacity) #returning edges with flow
+        return("edge: %s->%s; capacity: %s; flow: %s" %(self.vertex1, self.vertex2, self.capacity)) #returning edges with flow
 
 
 class fordFulkersonMaxFlow(object):
@@ -13,16 +17,39 @@ class fordFulkersonMaxFlow(object):
     edgeFlow={}
 
     def edgeAdd(self, vertex1, vertex2, edgeCapacity):
-        if(vertex1 != vertex2):
-            forwardEdge = createEdge(vertex1, vertex2, capacity) #creating forward edges
-            reverseEdge = createEdge(vertex2, vertex1, 0) #creating back edges
-            forwardEdge.reverseEdge = forwardEdge # the back edge is a forward edge for a reverse network
-            self.adjascent[vertex1].append(forwardEdge) #adding a forward edge from vertex1 to adjascent vertices
-            self.adjascent[vertex2].append(reverseEdge) #adding a reverse edge from the adjascent vertices to vertex1
-            self.flow[forwardEdge], self.flow[reverseEdge] = 0,0 #initially flow through every edge is 0
-        else: #both vertices are same.
-            print("ERROR: check graph as loop cannot occur")
-            exit()
+        forwardEdge = Edge(vertex1, vertex2, capacity) #creating forward edges
+        reverseEdge = Edge(vertex2, vertex1, 0) #creating back edges
+        forwardEdge.reverseEdge = forwardEdge # the back edge is a forward edge for a reverse network
+        self.adjascent[vertex1].append(forwardEdge) #adding a forward edge from vertex1 to adjascent vertices
+        self.adjascent[vertex2].append(reverseEdge) #adding a back edge from the adjascent vertices to vertex1
+        self.flow[forwardEdge], self.flow[reverseEdge] = 0,0 #initially flow through every edge is 0
 
     def vertexAdd(self, vertex):
         self.adjascent[vertex]=[] #list of vertices adjascent ot given vertices
+
+    def getEdge(self, vertex):
+        return(self.adjascent[vertex])
+
+    def searchPath(self, source, sink, path):
+        if(source==sink):
+            return(path)
+        for(edge in getEdge(source)):
+            resflow = Edge.edgeCapacity - self.flow[edge] #calculating residual flow for each edge
+            #initially residual flow is capacity of edge itself as flow is 0.
+
+            if(resflow>0 and edge not in path):
+                searchPath(edge.sink, sink, path+[edge])#adding that edge to current path
+                if(result!=None):
+                    return(path)
+
+
+
+
+
+'''
+add_edge('s','a',9)
+add_edge('s','c',9)
+add_edge('a','t',10)
+add_edge('c','t',7)
+(max_flow('s','t')
+'''
